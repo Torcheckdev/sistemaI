f838c628f181d772d1326a745a225ddc533d8395
RychRychh <rychclaw267@gmail.com>
2021-12-05 13:56:20 -0600

    modificacion de inserts asig,horario,materia,profe,hap

---
+++ models/sqlscripts/13inserthistorialpasado.sql
@@ -0,0 +1,7 @@
+INSERT INTO historialacademico(NumCuenta,folioAsig,IDmateria,Periodo,Calificacion,TipoExamen)VALUES(10030,30821001,100,"2018-1",5,"ORD");
+INSERT INTO historialacademico(NumCuenta,folioAsig,IDmateria,Periodo,Calificacion,TipoExamen)VALUES(10030,30821002,101,"2018-1",6,"ORD");
+INSERT INTO historialacademico(NumCuenta,folioAsig,IDmateria,Periodo,Calificacion,TipoExamen)VALUES(10030,30821003,102,"2018-1",7,"ORD");
+INSERT INTO historialacademico(NumCuenta,folioAsig,IDmateria,Periodo,Calificacion,TipoExamen)VALUES(10030,30821004,104,"2018-1",8,"ORD");
+INSERT INTO historialacademico(NumCuenta,folioAsig,IDmateria,Periodo,Calificacion,TipoExamen)VALUES(10030,30821005,105,"2018-1",9,"ORD");
+INSERT INTO historialacademico(NumCuenta,folioAsig,IDmateria,Periodo,Calificacion,TipoExamen)VALUES(10030,30821006,106,"2018-1",10,"ORD");
+INSERT INTO historialacademico(NumCuenta,folioAsig,IDmateria,Periodo,Calificacion,TipoExamen)VALUES(10030,30821007,107,"2018-1",0,"ORD");
