7adf386008ed914f493151156dc136c309c0022f
unknown <branxdxx999.gmail.com>
2021-12-16 16:52:39 -0600

    arregle bug de git

--- Client/src/Components/Inscripicion.js
+++ Client/src/Components/Inscripicion.js
@@ -99,14 +99,10 @@ function Inscripcion({NumCuenta,Periodo,planEstudios}) {
                   }, {Creditos: 0});
 
                   console.log(creditos);
-<<<<<<< HEAD
-                  if( creditos.Creditos > 50){
-=======
-               /*   if( creditos.Creditos > 50){
->>>>>>> main
+                  /*if( creditos.Creditos > 50){
                     alert.show("No puedes inscribirte porque sobre pasas los 50 creditos");
                     return;
-                  }
+                  }*/
 
                   const arregloIncripcionMaterias = materiasInscrbir.map(materia =>{
                         return {NumCuenta:NumCuenta.toString() ,folioAsig:materia.folioAsig.toString(),Periodo:Periodo,Calificacion:"0",TipoExamen:"ORD",IDmateria:materia.IDmateria.toString() }
